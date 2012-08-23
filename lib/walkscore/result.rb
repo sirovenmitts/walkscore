fa1c89774e951372441e5a707e7141acb94a30a5
Kenneth Pullen <kenneth.pullen@gmail.com>
2012-08-23 01:48:57 -0700

    Change _url to _uri
    Halfway through writing the README I got this brilliant idea: Since
    all the URIs returned by Walkscore::Result are URI objects, their
    names should use _uri instead of their current _url. I forgot to
    actually update the Walkscore::Result class though.

--- lib/walkscore/result.rb
+++ lib/walkscore/result.rb
@@ -1,24 +1,26 @@
 require 'uri'
+require 'time'
+
 module Walkscore
 	class Result
 		attr_reader(
 			:score,
 			:description,
 			:updated_at,
-			:score_url,
-			:walkscore_logo_url,
-			:more_info_icon_url,
-			:more_info_url)
+			:score_uri,
+			:walkscore_logo_uri,
+			:more_info_icon_uri,
+			:more_info_uri)
 		def initialize attributes
 			@score = attributes['walkscore']
 			@description = attributes['description']
 
 			@updated_at = Time.parse attributes['updated']
 
-			@score_url = URI(attributes['ws_link'])
-			@walkscore_logo_url = URI(attributes['logo_url'])
-			@more_info_icon_url = URI(attributes['more_info_icon'])
-			@more_info_url = URI(attributes['more_info_link'])
+			@score_uri = URI(attributes['ws_link'])
+			@walkscore_logo_uri = URI(attributes['logo_url'])
+			@more_info_icon_uri = URI(attributes['more_info_icon'])
+			@more_info_uri = URI(attributes['more_info_link'])
 		end
 	end
 end
